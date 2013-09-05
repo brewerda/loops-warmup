10821ef1bb14731fbe951d27dbcce39c0b060a3c
brewerda <brewerda@s.dcsdk12.org>
2013-09-05 09:16:57 -0600

    Made Methods

---
+++ hw1.java
@@ -0,0 +1,16 @@
+public class hw1 {
+	public static void main(String[] args) {
+		3(3)
+
+	}
+	public void 3(int x) {
+		int sum = 1;
+		for (x=x;x > (1.0 * 10^-12) ; x++ ) {
+			sum = sum + x;
+
+		}
+	}
+	public void 5() {
+
+	}
+}
