aaa9eaa79b7037a2a9c9ddd3b327374402d7b919
brewerda <brewerda@s.dcsdk12.org>
2013-09-05 09:28:34 -0600

    Part of #5 working

--- hw1.java
+++ hw1.java
@@ -1,16 +1,33 @@
+import java.util.Scanner;
+
+
 public class hw1 {
 	public static void main(String[] args) {
-		3(3)
+		// 3(3);
+		5();
 
 	}
 	public void 3(int x) {
 		int sum = 1;
-		for (x=x;x > (1.0 * 10^-12) ; x++ ) {
+		for (x=x;x > Math.pow(10^-12) ; x++ ) {
 			sum = sum + x;
 
 		}
 	}
 	public void 5() {
+		int number;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a positive integer: ");
+		number = reader.nextInt();
+		while (number > 1) {
+			if (number%2 == 0 ) {
+				number = number/2;
+				System.out.println(number);
 
+			} else {
+				number = 3*number + 1;
+				System.out.println(number);
+			}
+		}
 	}
 }
