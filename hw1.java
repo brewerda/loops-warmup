296f3e25502872025d70d483fc0b5f8e4cb2e1de
brewerda <brewerda@s.dcsdk12.org>
2013-09-09 21:58:03 -0600

    Update hw1.java

--- hw1.java
+++ hw1.java
@@ -3,24 +3,24 @@ import java.util.Scanner;
 
 public class hw1 {
 	public static void main(String[] args) {
-		// 3(3);
-		5();
+		 3(3);
+		//5();
 
 	}
 	public void 3(int x) {
 		int sum = 1;
-		for (x=x;x > Math.pow(10^-12) ; x++ ) {
+		for (x=n;x > Math.pow(10^-12) ; Math.pow(e^(sum) / n ) {
 			sum = sum + x;
 
 		}
 	}
 	public void 5() {
-		int number;
+		long number;
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter a positive integer: ");
 		number = reader.nextInt();
-		while (number > 1) {
-			if (number%2 == 0 ) {
+		while (number > 1 ) {
+			if ((number%2) ==0 ) {
 				number = number / 2;
 				System.out.println(number);
 
